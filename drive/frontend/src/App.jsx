7095c5e5a6fc7150dd2a964f637192c01a4f3ca2
Jagadesh Babu Thangavelu <jagadeshbabu.t@gmail.com>
2018-11-18 12:28:28 +0530

    Add bin page and restore support

--- drive/frontend/src/App.jsx
+++ drive/frontend/src/App.jsx
@@ -126,7 +126,10 @@ class App extends Component {
                     </a>
                   </li>
                   <li className="nav-item">
-                    <a className="nav-link" href="#">
+                    <a
+                      className="nav-link"
+                      href="#"
+                      onClick={(e) => this.props.switchComponent('bin')}>
                       <span data-feather="file"></span>
                       Bin
                     </a>
@@ -236,6 +239,176 @@ class App extends Component {
                     'padding': '15px'
                   }}
                     onClick={(e) => this.deleteFolder(e, folder.delete_url)}>delete</a>
+                </div>
+              </div>)}
+              <h6
+                className="col-md-12"
+                style={{
+                "marginBottom": "20px"
+              }}>Files</h6>
+              {!files.length && <p style={{
+                'paddingLeft': '15px'
+              }}>No files present in this folder</p>}
+              {files.map((file) => <div
+                className="col-md-12"
+                key={file.id}
+                style={{
+                "marginBottom": "30px"
+              }}>
+                <a href={this.state.domain + file.url}>{file.name}</a>
+              </div>)}
+            </main>
+          </div>
+        </div>
+      </Fragment>
+    );
+  }
+}
+
+export class BinApp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      url: '/store/bin/',
+      value: '',
+      selectedFile: null,
+      loaded: 0,
+      search: '',
+      folders: [],
+      files: [],
+      domain: 'http://localhost:8000'
+    };
+  }
+
+  folder = event => {
+    this.setState({value: event.target.value});
+    console.log(this.state.value);
+  };
+
+  file = event => {
+    this.setState({
+      selectedFile: event.target.files[0],
+      loaded: 0
+    }, () => {
+      this.addFile();
+    });
+  };
+
+  navigate = (event, url) => {
+    this.setState({
+      url: url
+    }, () => {
+      this.drive();
+    });
+  }
+
+  restoreFolder = (event, url) => {
+    url = this.state.domain + url
+    axios
+      .delete(url)
+      .then(res => {
+        console.log(res.statusText);
+        this.componentDidMount();
+      })
+  }
+
+  drive = () => {
+    let url = this.state.domain + this.state.url;
+    axios
+      .get(url)
+      .then(res => {
+        console.log(res.data);
+        this.setState({files: res.data.files, folders: res.data.folders});
+      })
+  }
+
+  componentDidMount = () => {
+    this.drive();
+  }
+
+  render() {
+    const files = this.state.files;
+    const folders = this.state.folders;
+    return (
+      <Fragment>
+        <nav className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0">
+          <input
+            className="form-control form-control-dark w-100"
+            type="text"
+            onChange={(e) => this.search(e)}
+            placeholder="Search"
+            aria-label="Search"/>
+        </nav>
+
+        <div className="container-fluid">
+          <div className="row">
+            <nav className="col-md-2 d-none d-md-block bg-light sidebar">
+              <div className="sidebar-sticky">
+                <ul className="nav flex-column">
+                  <li className="nav-item">
+                    <a
+                      className="nav-link"
+                      href="#"
+                      onClick={(e) => this.props.switchComponent('home')}>
+                      <span data-feather="home"></span>
+                      Home
+                      <span className="sr-only">(current)</span>
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link active" href="#">
+                      <span data-feather="file"></span>
+                      Bin
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </nav>
+            <main role="main" className="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4 row">
+              <h5
+                className="col-md-12"
+                style={{
+                "marginBottom": "30px"
+              }}>Drive</h5>
+              <h6 className="col-md-12">Bin</h6>
+            </main>
+            <main role="main" className="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4 row">
+              <h6
+                className="col-md-12"
+                style={{
+                "marginBottom": "20px"
+              }}>Folders</h6>
+              {!folders.length && <p style={{
+                'paddingLeft': '15px'
+              }}>No folders present in this folder</p>}
+              {folders.map((folder) => <div
+                className="col-md-2"
+                key={folder.id}
+                style={{
+                "marginBottom": "30px"
+              }}>
+                <button
+                  className="btn btn-lg btn-outline-success"
+                  style={{
+                  "width": "100%",
+                  "overflow": "hidden",
+                  'borderBottomLeftRadius': '0',
+                  'borderBottomRightRadius': '0'
+                }}
+                  onClick={(e) => this.navigate(e, folder.url)}>{folder.name}</button>
+                <div
+                  className="btn btn-success"
+                  style={{
+                  'padding': '0',
+                  'borderTopLeftRadius': '0',
+                  'borderTopRightRadius': '0',
+                  'width': '100%'
+                }}>
+                  <a
+                    style={{
+                    'padding': '15px'
+                  }}
+                    onClick={(e) => this.restoreFolder(e, folder.restore_url)}>Restore</a>
                 </div>
               </div>)}
               <h6
--- drive/frontend/src/index.jsx
+++ drive/frontend/src/index.jsx
@@ -1,12 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App, {BinApp} from './App';
+import React, {Fragment} from 'react';
+
+import ReactDOM from 'react-dom';
+
+class Base extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      component: 'home'
+    }
+    this.switchComponent = this
+      .switchComponent
+      .bind(this);
+  }
+
+  switchComponent(component) {
+    this.setState({component: component})
+  }
+
+  render() {
+    return (
+      <Fragment>
+        {this.state.component === 'home'
+          ? <App switchComponent={this.switchComponent}/>
+          : <BinApp switchComponent={this.switchComponent}/>}
+      </Fragment>
+    )
+  }
+}
+
+ReactDOM.render(
+  <Base/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// unregister() to register() below. Note this comes with some pitfalls. Learn
+// more about service workers: http://bit.ly/CRA-PWA
